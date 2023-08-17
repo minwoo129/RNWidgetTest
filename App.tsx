8a674844c58d588378dead1e8e4938d00e5cc9e9
minwoo129 <key0129mw@naver.com>
2023-08-17 09:41:18 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>테스트</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
