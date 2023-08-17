852b7dea8985409c90ae829258cd7e41b22aa09f
minwoo129 <key0129mw@naver.com>
2023-08-17 09:45:19 +0900

    위젯에 적용될 데이터 구조 추가(테스트)

--- App.tsx
+++ App.tsx
@@ -5,11 +5,18 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 function App(): JSX.Element {
+  const [test1, setTest1] = useState('');
+  const [test2, setTest2] = useState('');
+
+  const widgetData = {
+    test1,
+    test2,
+  };
   return (
     <SafeAreaView style={styles.container}>
       <Text>테스트</Text>
